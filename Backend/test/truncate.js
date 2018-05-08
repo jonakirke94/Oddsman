54cfe024f5944e21796976e744a9579cfa4d62a8
sebastian gram <swkgram@gmail.com>
2018-05-08 09:35:42 +0200

    changed object.values to object.keys

--- Backend/test/truncate.js
+++ Backend/test/truncate.js
@@ -22,62 +22,61 @@ db.sequelize.query("set FOREIGN_KEY_CHECKS=0").then(
 exports.clear = (done) => {
 
 
-    let multiQueryArr = [];    
-    multiQueryArr.push('SET FOREIGN_KEY_CHECKS = 0;');
-    Object.values(db.sequelize.models).forEach(element => {
-      multiQueryArr.push('TRUNCATE '+ element.name + ";");
-    });
-    multiQueryArr.push('SET FOREIGN_KEY_CHECKS = 1;');
+  let multiQueryArr = [];
+  multiQueryArr.push('SET FOREIGN_KEY_CHECKS = 0;');
+  Object.keys(db.sequelize.models).forEach(key => {
+    multiQueryArr.push('TRUNCATE ' + db.sequelize.models[key].name + ";");
+  });
+  multiQueryArr.push('SET FOREIGN_KEY_CHECKS = 1;');
 
-    db.sequelize.query(multiQueryArr.join(' ')).then(function(){
-      done();
-    })
+  db.sequelize.query(multiQueryArr.join(' ')).then(function () {
+    done();
+  })
 }
-    
 
-    /* Object.values(db.sequelize.models).map(function(model) {
-      return model.destroy({ truncate: true });
-    });  */
-    //sequelize.query();
- 
-    /* db.sequelize
-      .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
+
+/* Object.values(db.sequelize.models).map(function(model) {
+  return model.destroy({ truncate: true });
+});  */
+//sequelize.query();
+
+/* db.sequelize
+  .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
+  .then(
+    usertournament
+      .destroy({ truncate: true, force: true, logging: false })
       .then(
-        usertournament
-          .destroy({ truncate: true, force: true, logging: false })
+        db.sequelize
+          .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
           .then(
-            db.sequelize
-              .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
+            request
+              .destroy(
+                { truncate: true, force: true, logging: false }
+         
+              )
               .then(
-                request
-                  .destroy(
-                    { truncate: true, force: true, logging: false }
-             
-                  )
+                db.sequelize
+                  .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
                   .then(
-                    db.sequelize
-                      .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
+                    user
+                      .destroy(
+                        { truncate: true, force: true, logging: false }
+              
+                      )
                       .then(
-                        user
-                          .destroy(
-                            { truncate: true, force: true, logging: false }
-                  
-                          )
+                        db.sequelize
+                          .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
                           .then(
-                            db.sequelize
-                              .query("set FOREIGN_KEY_CHECKS=0", {logging: false})
-                              .then(
-                                tournament
-                                  .destroy(
-                                    { truncate: true, force: true, logging: false }
-                                  
-                                  )
-                                  .then(done())
+                            tournament
+                              .destroy(
+                                { truncate: true, force: true, logging: false }
+                              
                               )
+                              .then(done())
                           )
                       )
                   )
               )
           )
-      );  */  
- 
+      )
+  );  */
