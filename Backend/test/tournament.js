497853d0287741c37669c7f055952b8549c2fdea
swkgram <swkgram@gmail.com>
2018-05-12 17:25:28 +0200

    fixed NOT get current tournament test.

--- Backend/test/tournament.js
+++ Backend/test/tournament.js
@@ -258,7 +258,7 @@
            });
          });
      });
-     it("It should try to get the current tournament but wont find any", done => {
+     it("It should NOT get the current tournament", done => {
        const user = helper.getUser();
        chai.request(server)
          .post("/user/signup") //ENDPOINT[2]
