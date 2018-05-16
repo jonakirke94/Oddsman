8c789426b976e5106b8a3a8ef150f57e7017d253
sebastian gram <swkgram@gmail.com>
2018-05-16 12:01:51 +0200

    added table with missing matches and editing capabilities

--- Frontend/oddsman/src/app/adminpanel/admincomponents/edit-match/edit-match.component.ts
+++ Frontend/oddsman/src/app/adminpanel/admincomponents/edit-match/edit-match.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { MatchService, Match } from '../../../services/match.service';
 
 @Component({
-  selector: 'app-edit-match',
-  templateUrl: './edit-match.component.html',
-  styleUrls: ['./edit-match.component.sass']
+    selector: 'app-edit-match',
+    templateUrl: './edit-match.component.html',
+    styleUrls: ['./edit-match.component.sass']
 })
 export class EditMatchComponent implements OnInit {
 
-  matches: Match[];
+    matches: Match[];
 
-  displayDialog: boolean;
+    displayDialog: boolean;
 
-  match: Match = {};
+    match: Match = {};
 
     selectedMatch: Match;
 
@@ -23,66 +23,70 @@ export class EditMatchComponent implements OnInit {
     cols: any[];
 
 
-  constructor(private _match : MatchService) { }
+    constructor(private _match: MatchService) { }
 
-  ngOnInit() {
-    this.loadMatches();
+    ngOnInit() {
+        this.loadMatches();
 
-    this.cols = [
-      { field: 'Id', header: 'Id' },
-      { field: 'MatchName', header: 'Kamp' },
-    ];
-  }
-
-  ngOnDestroy() {
-
-  }
-
-  loadMatches() {
-    this._match.getMissingMatches().subscribe(res => {
-      this.matches = res;
-      console.log(this.matches)
-    })
-  }
-
-  showDialogToAdd() {
-    this.newMatch = true;
-    this.match = {};
-    this.displayDialog = true;
-}
-
-save() {
-/*     let cars = [...this.cars];
-    if (this.newCar)
-        cars.push(this.car);
-    else
-        cars[this.cars.indexOf(this.selectedCar)] = this.car;
-
-    this.cars = cars;
-    this.car = null; */
-    this.displayDialog = false;
-}
-
-/* delete() {
-    let index = this.cars.indexOf(this.selectedCar);
-    this.cars = this.cars.filter((val, i) => i != index);
-    this.car = null;
-    this.displayDialog = false;
-} */
-
-onRowSelect(event) {
-    this.newMatch = false;
-    this.match = this.cloneMatch(event.data);
-    this.displayDialog = true;
-}
-
-cloneMatch(m: Match): Match {
-    let match = {};
-    for (let prop in m) {
-        match[prop] = m[prop];
+        this.cols = [
+            { field: 'Id', header: 'Id' },
+            { field: 'MatchId', header: 'Kamp nr.' },
+            { field: 'MatchName', header: 'Kamp' },
+            { field: 'MatchDate', header: 'Start' },
+            { field: 'Option1Odds', header: '1' },
+            { field: 'Option2Odds', header: 'X' },
+            { field: 'Option3Odds', header: '2' },
+        ];
     }
-    return match;
-}
+
+    ngOnDestroy() {
+
+    }
+
+    loadMatches() {
+        this._match.getMissingMatches().subscribe(res => {
+            this.matches = res;
+            console.log(this.matches)
+        })
+    }
+
+    showDialogToAdd() {
+        this.newMatch = true;
+        this.match = {};
+        this.displayDialog = true;
+    }
+
+    save() {
+        this._match.updateMatch(this.selectedMatch.Id, this.match).subscribe(res => {
+            this.loadMatches();
+        });
+        this.displayDialog = false;
+    }
+
+    isMatchDate(col) {
+        return col.field === "MatchDate";
+    }
+
+    /* delete() {
+        let index = this.cars.indexOf(this.selectedCar);
+        this.cars = this.cars.filter((val, i) => i != index);
+        this.car = null;
+        this.displayDialog = false;
+    } */
+
+    onRowSelect(event) {
+        this.newMatch = false;
+        this.match = this.cloneMatch(event.data);
+        this.displayDialog = true;
+    }
+
+    cloneMatch(m: Match): Match {
+        let match = {};
+        for (let prop in m) {
+            match[prop] = m[prop];
+        }
+        return match;
+    }
 
 }
 
--- Frontend/oddsman/src/app/services/match.service.ts
+++ Frontend/oddsman/src/app/services/match.service.ts
@@ -10,8 +10,13 @@ export class MatchService {
   getMissingMatches() {
     return this.http
       .get("http://localhost:3000/match/missing")
-      .map(res => <Match[]> res['data'])
+      .map(res => <Match[]>res['data'])
       .shareReplay();
+  }
+
+  updateMatch(id, match) {
+    console.log("service - " + JSON.stringify(match));
+    return this.http.patch(`http://localhost:3000/match/${id}`, match).map(res => res);
   }
 
 }
