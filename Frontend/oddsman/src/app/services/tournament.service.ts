b0ff2fbe9cc068800ab91a76d608cc84ce0e4578
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-05-20 14:17:08 +0200

    cleaned services

--- Frontend/oddsman/src/app/services/tournament.service.ts
+++ Frontend/oddsman/src/app/services/tournament.service.ts
@@ -64,7 +64,6 @@ export class TournamentService {
     .map(res => res);
   }
 
-  //fetches the users current active tournament
   getCurrentUserTournament() {
     return this.http
     .get(`${this.baseUrl}/current/user`)
