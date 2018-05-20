5c28c5b52fe8acf6476a9e27bdd0aaabffa2cfe6
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-05-20 17:21:30 +0200

    added a feature module for dashboard module

--- Frontend/oddsman/src/app/shared/shared.module.ts
+++ Frontend/oddsman/src/app/shared/shared.module.ts
@@ -5,6 +5,15 @@ import {  ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
 import { HttpClientModule } from '@angular/common/http';
 
+/* Custom components that can be used in any module */
+import { HistoryComponent } from '../history/history.component';
+import { StandingComponent } from '../standing/standing.component';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { ResultsComponent } from '../adminpanel/admincomponents/results/results.component';
+import { ResultFeedComponent } from '../result-feed/result-feed.component';
+import { BetFeedComponent } from '../bet-feed/bet-feed.component';
+
+
 @NgModule({
   imports: [
     CommonModule,
@@ -20,9 +29,21 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     ReactiveFormsModule,
     ChartsModule,
-    HttpClientModule
- 
+    HttpClientModule,
+    HistoryComponent,
+    StandingComponent,
+    LoadingSpinnerComponent,     
+    BetFeedComponent,
+    ResultFeedComponent,
+    ResultsComponent
   ],
-  declarations: []
+  declarations: [
+    HistoryComponent,
+    StandingComponent,
+    LoadingSpinnerComponent,     
+    BetFeedComponent,
+    ResultFeedComponent,
+    ResultsComponent
+  ]
 })
 export class SharedModule { }
