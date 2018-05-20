b937a7a35bbf72a41cfd75224bf1c329013a640f
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-05-20 15:23:24 +0200

    added services to core module

--- Frontend/oddsman/src/app/app.module.ts
+++ Frontend/oddsman/src/app/app.module.ts
@@ -4,24 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { AuthService } from './services/auth.service';
 import { AuthInterceptor } from './auth.interceptor';
-import { OddsService } from './services/odds.service';
 import { AuthGuard } from './guards/auth.guard';
 import { AdminguardGuard } from './guards/adminguard.guard';
-
-
-
-import { UserService } from './services/user.service';
-import { TournamentService } from './services/tournament.service';
-
-
-
-
-import { MatchService } from './services/match.service';
-import { HomeComponent } from './home/home.component';
 import * as io from 'socket.io-client';
-import { SocketService } from './services/socket.service';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 
