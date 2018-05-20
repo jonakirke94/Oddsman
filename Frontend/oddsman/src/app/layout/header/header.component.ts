23f510b19e4b27d69f5bcb05b250c805ad744bfd
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-05-20 15:57:18 +0200

    refactored header/footer into own components and added them to core module

---
+++ Frontend/oddsman/src/app/layout/header/header.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'layout-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+
+  toggleNav: false;
+  isLoggedin$: Observable<boolean>;
+  isAdmin$: Observable<boolean>;
+
+
+  constructor(private _auth: AuthService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.setHeader();
+  }
+
+  private setHeader() {
+    const loggedIn = this._auth.isLoggedIn();
+    if (loggedIn) {
+      this.isLoggedin$ = loggedIn;
+    }
+
+    const isAdmin = this._auth.isLoggedAsAdmin();
+    if (isAdmin) {
+      this.isAdmin$ = isAdmin;
+    }
+
+  }
+
+  logout() {
+    this._auth.logout();
+    this.router.navigateByUrl("/login");
+  }
+
+}
