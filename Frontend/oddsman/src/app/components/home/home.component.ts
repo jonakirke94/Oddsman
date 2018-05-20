9aceff652213d4e97c05ab31fb92a8f9db081c72
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-05-20 19:05:10 +0200

    refactored single use components and added info/statistics template components

--- Frontend/oddsman/src/app/components/home/home.component.ts
+++ Frontend/oddsman/src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { StandingComponent } from "../shared/standing/standing.component";
-import { BetFeedComponent} from '../shared/bet-feed/bet-feed.component';
-import { ResultFeedComponent} from '../shared/result-feed/result-feed.component';
+import { StandingComponent } from "../../shared/standing/standing.component";
+import { BetFeedComponent} from '../../shared/bet-feed/bet-feed.component';
+import { ResultFeedComponent} from '../../shared/result-feed/result-feed.component';
 
 @Component({
   selector: 'app-standing',
@@ -20,7 +20,5 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   
   }
-
-
 
 }
