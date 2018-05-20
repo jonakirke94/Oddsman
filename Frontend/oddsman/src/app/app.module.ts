676acde30ced5e90d5ddec4827e071747527ba84
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-05-20 15:26:00 +0200

    added guards to core module

--- Frontend/oddsman/src/app/app.module.ts
+++ Frontend/oddsman/src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { AuthInterceptor } from './auth.interceptor';
-import { AuthGuard } from './guards/auth.guard';
-import { AdminguardGuard } from './guards/adminguard.guard';
 import * as io from 'socket.io-client';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-
-
 
 @NgModule({
   declarations: [
@@ -25,14 +19,6 @@ import { CoreModule } from './core.module';
     CoreModule 
   ],
   providers: [
-    AuthGuard,
-    AdminguardGuard,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
-
   ],
   bootstrap: [AppComponent]
 })
